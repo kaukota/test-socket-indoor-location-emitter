35a108b4a61950ea7d1d53346400646d1247341d
Maxime Colmant <maxime.colmant@gmail.com>
2018-10-30 15:13:37 +0100

    fix: use JSON strings in configuration to parse it correctly via environment

--- config/env/all.js
+++ config/env/all.js
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = {
-	port: process.env.PORT || 3003,
-
-	bbox: process.env.bbox || [3.019240, 50.632392, 3.021464, 50.633634],
-	floorbox: process.env.floorbox || [0, 3]
+  port: process.env.PORT || 3003,
+  bbox: process.env.bbox ? JSON.parse(process.env.bbox) : [3.01924, 50.632392, 3.021464, 50.633634],
+  floorbox: process.env.floorbox ? JSON.parse(process.env.floorbox) : [0, 3]
 };
